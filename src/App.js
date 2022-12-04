da2e55775ee68b028e015add5bcf5b59b9911b6b
Nitin Bisht <3498georgian@gmail.com>
2022-12-04 20:11:05 +0530

    Refactor code to change inline functions to class components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const testData = [
   { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
@@ -8,42 +8,51 @@ const testData = [
   { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
 ];
 
-
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <HeaderTitle />
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <HeaderTitle />
+        </div>
+        <div className='cardArea'>
+          <CardList profileList={testData} />
+        </div>
       </div>
-      <div className='cardArea'>
-        <CardList profileList={testData}/>
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
 
 function HeaderTitle() {
   return <div>Github Cards Project</div>
 }
 
-const CardList = (props) => {
-  return (
-    <div>
-      {props.profileList.map((profile) => <Card key={profile.id} {...profile} />)}
-    </div>
-  )
+class CardList extends React.Component {
+  state = [];
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        {this.props.profileList.map((profile) => <Card key={profile.id} {...profile} />)}
+      </div>
+    )
+  }
 }
 
-function Card(props) {
-  return (
-    <div className='github-profile'>
-      <img src={props.avatar_url} alt='Good Vibes' />
-      <div className='info'>
-        <div className='name'> Name : {props.name}</div>
-        <div className='company'> Company : {props.company}</div>
+class Card extends React.Component {
+  render() {
+    const profile = this.props;
+    return (
+      <div className='github-profile'>
+        <img src={profile.avatar_url} alt='Good Vibes' />
+        <div className='info'>
+          <div className='name'> Name : {profile.name}</div>
+          <div className='company'> Company : {profile.company}</div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App;
